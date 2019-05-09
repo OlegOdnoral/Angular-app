5feb8f785f96015084acd0efdb35d9384601d066
Oleg Odnoral <4uiiakabpa@gmail.com>
2019-05-09 18:20:02 +0300

    Fix Testing issues

--- apps/rank-active-e2e/src/app.po.ts
+++ apps/rank-active-e2e/src/app.po.ts
@@ -6,6 +6,6 @@ export class AppPage {
   }
 
   getTitleText() {
-    return element(by.css('angular-app-root h1')).getText() as Promise<string>;
+    return element(by.css('app-rank-active-root h1')).getText() as Promise<string>;
   }
 }
